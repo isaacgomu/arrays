2a46e7ebc11ce888a817ea9154e0b7fbf4cdc9e2
isaacgomu <IsaacDalglish03@gmail.com>
2024-03-25 13:52:15 +0000

    did 4 arrays

---
+++ app.js
@@ -0,0 +1,46 @@
+console.log("hello world");
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
+
+console.log(blogPosts);
+
+const favouriteFoods = [
+  "My first favourite food is pizza",
+  "My second favourite food is ramen",
+  "My third favourite food is curry",
+];
+
+console.log(favouriteFoods[0]);
+console.log(favouriteFoods[1]);
+console.log(favouriteFoods[2]);
+
+const favouriteColours = [
+  "My first favourite colour is purple",
+  "My second favourite colour is green",
+  "My third favourite colour is orange",
+];
+
+console.log(favouriteColours[0]);
+console.log(favouriteColours[1]);
+console.log(favouriteColours[2]);
+
+const favouriteNumbers = [
+  "My first favourite number is 1937",
+  "My second favourite number is 56",
+  "My third favourite number is 1044",
+];
+
+console.log(favouriteNumbers[0]);
+console.log(favouriteNumbers[1]);
+console.log(favouriteNumbers[2]);
